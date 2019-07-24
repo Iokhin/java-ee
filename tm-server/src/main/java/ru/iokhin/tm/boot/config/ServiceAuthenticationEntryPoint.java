3ae1f3a68f2885116bb4b559748f3e06ae327ce0
iokhin <iokhin@i-teco.ru>
2019-07-24 13:06:15 +0300

    SP-07-08 Забытые файлы.

---
+++ tm-server/src/main/java/ru/iokhin/tm/boot/config/ServiceAuthenticationEntryPoint.java
@@ -0,0 +1,19 @@
+package ru.iokhin.tm.boot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+public class ServiceAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
